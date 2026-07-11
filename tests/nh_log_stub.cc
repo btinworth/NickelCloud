c588904970652d83139107784e5601ee723c06fb
Ben Tinworth <ben.tinworth@gmail.com>
2026-07-11 19:32:05 +1000

    Update test action, disable test logging

--- tests/nh_log_stub.cc
+++ tests/nh_log_stub.cc
@@ -1,12 +1,3 @@
 #include <NickelHook.h>
-#include <cstdarg>
-#include <cstdio>
 
-void nh_log(const char* fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    va_end(args);
-    fprintf(stderr, "\n");
-}
+void nh_log(const char*, ...) {}
